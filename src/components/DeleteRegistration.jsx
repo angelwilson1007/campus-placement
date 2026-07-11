f658c1733cd55be44062b4b0569efc848dcc1934
angelwilson1007 <angelwilson1007@gmail.com>
2026-07-11 13:10:25 +0530

    nav

--- src/components/DeleteRegistration.jsx
+++ src/components/DeleteRegistration.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavRegistration from './NavRegistration'
 
 const DeleteRegistration = () => {
     return (
         <div>
-           
+           <NavRegistration/>
             <div className="container">
                 <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row">
